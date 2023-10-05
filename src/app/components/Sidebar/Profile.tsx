f2b25e9f7eecaa01e19f1b753f1fb6c768b0c7a2
Yuri Pires <yuripiresbr@gmail.com>
2023-10-05 11:28:53 -0300

    feat: created Profile component

---
+++ src/app/components/Sidebar/Profile.tsx
@@ -0,0 +1,22 @@
+import { LogOut } from "lucide-react";
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="https://github.com/yuripiresg.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-1 flex-col">
+        <span className="text-sm font-semibold text-zinc-700">Yuri Pires</span>
+        <span className="truncate text-sm text-zinc-500">
+          yuripiresbr@gmail.com.br
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  );
+}
